9592986c3aa9a860ef5968df6bc68e71161df4e2
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-11-09 14:19:51 -0600

    04_removeFromArray first test now passes

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, remove) {
+    let resultArray = [];
 
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== remove) {
+            resultArray.push(arr[i]);
+        }
+    }
+    return resultArray;
 };
 
 // Do not edit below this line
