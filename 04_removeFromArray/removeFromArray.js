8a16f2f573317b78b413971794c98397939f8158
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-11-09 15:03:19 -0600

    Completed: 04_removeFromArray, all tests pass

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,13 +1,25 @@
-const removeFromArray = function(arr, remove) {
-    let resultArray = [];
+const removeFromArray = (arr, ...args) => {
+    let newArr = [...arr];
+    
+     for (let i = 0; i < args.length; i++) {
+        for (let j = 0; j < newArr.length; j++) {
+            if (args[i] === newArr[j]) {
+                newArr.splice(j, 1);
+            }
+        }
+    }
 
-    for (let i = 0; i < arr.length; i++) {
+/*     for (let i = 0; i < arr.length; i++) {
         if (arr[i] !== remove) {
             resultArray.push(arr[i]);
         }
-    }
-    return resultArray;
+    } */
+    console.log(newArr);
+    return newArr;
 };
+
+removeFromArray([3,2,5], 3,2,4,4);
+removeFromArray([76,5], 5,75,76);
 
 // Do not edit below this line
 module.exports = removeFromArray;
