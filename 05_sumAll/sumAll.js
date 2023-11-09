0668c6d1ed849c90729f522d69103fb118d87bc3
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-11-09 17:31:53 -0600

    Completed: 05_sumAll, all tests pass

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    
+    let lowNum;
+    let highNum;
+    let sum = 0;
 
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR";
+    }
+
+    if (a > b) {
+        highNum = a;
+        lowNum = b;
+    } else {
+        highNum = b;
+        lowNum = a;
+    }
+
+    for (i = lowNum; i <= highNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
