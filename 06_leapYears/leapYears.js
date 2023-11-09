1a053e9d960268ceece1994a5abe4bfce3340a90
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-11-09 17:46:00 -0600

    Completed: 06_leapYears, all test pass

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    if (year % 100 === 0) {
+        if (year % 400 === 0) {
+            return true;
+        }
+        return false;
+    } else if (year % 4 === 0) {
+        return true;
+    }
+    return false;
 };
 
+console.log(leapYears(700));
+console.log(leapYears(701));
+console.log(leapYears(1600));
 // Do not edit below this line
 module.exports = leapYears;
