2f7a9236d76220189e3d2a4775b0227c50d2591e
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-11-09 13:50:37 -0600

    Completed: 02_repeatString, all tests pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let repeatedString = "";
+    if (str === "") {
+        return str;
+    }
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++) {
+        repeatedString += str;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
