6e7a665a775138c16c56920eb6ae3f6fe63e6f62
GreatDistances <117937385+GreatDistances@users.noreply.github.com>
2023-11-19 20:25:35 -0600

    Completed: 11_getTheTitles exercise, all tests pass

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arrOfObj) {
+    let arr = [...arrOfObj];
+    const titles = arr.map((item) => {
+        return item.title;
+    })
+    return titles;
 };
+
+const books1 = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+
+console.log(getTheTitles(books1));
 
 // Do not edit below this line
 module.exports = getTheTitles;
